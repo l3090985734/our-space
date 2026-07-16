06d8c93abc1fe2218526de7da47067285c7feb9c
廉荣捷 <lianrongjie@lianrongjies-MacBook-Air.local>
2026-07-16 15:36:53 +0800

    修复：移除页面切换动画，解决目标页空白bug

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,24 +1,11 @@
-import { Outlet, useLocation } from 'react-router-dom'
-import { motion, AnimatePresence } from 'framer-motion'
+import { Outlet } from 'react-router-dom'
 import { BottomNav } from './BottomNav'
 
 export function Layout() {
-  const location = useLocation()
-
   return (
     <div className="min-h-screen bg-gradient-to-b from-sakura-light to-white">
       <main className="max-w-lg mx-auto px-4 pt-6 pb-24">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, x: 20 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -20 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Outlet />
-          </motion.div>
-        </AnimatePresence>
+        <Outlet />
       </main>
       <BottomNav />
     </div>
